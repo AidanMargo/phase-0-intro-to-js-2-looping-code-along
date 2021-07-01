c47068660d88aea83b790aac15ec4a2783a5f167
Aidan Margo <aidanmargo@gmail.com>
2021-07-01 15:51:33 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+    const messages = [];
+    for(let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        
+    }   
+    return messages;
+}
+
+ let num = 10
+function countDown(num) {
+    while (num >= 0) {
+    console.log(num);
+    num--;
+    }
+    
+}
+
